9ff9a984e33337ba175c48d923093b9adceb3a10
manodhiambo <manodhiambo@gmail.com>
2024-08-29 04:35:08 +0300

    Committing on shape printer

---
+++ ShapePrinter.java
@@ -0,0 +1,22 @@
+public class ShapePrinter
+{
+
+	public static void main(String [] args){
+	
+		printPattern();
+	}
+
+
+		public static void printPattern(){
+	
+		System.out.println("*********");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*********");
+	}
+}
