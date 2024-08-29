43cfedf692587307636f623ea311b22308b62d6b
manodhiambo <manodhiambo@gmail.com>
2024-08-29 20:58:38 +0300

    Committing first attempts in OOB

---
+++ GradeBook.java
@@ -0,0 +1,12 @@
+// Class declaration with one method
+
+public class GradeBook
+{
+
+	// display a welcom message to the gradebook user
+	public void displayMessage()
+	{
+	
+		System.out.println("Welcome to the Grade Book");
+	}
+}
---
+++ GradeBookTest.java
@@ -0,0 +1,13 @@
+// Creating a gradeBook object and calling its displayMessage method
+
+public class GradeBookTest
+{
+
+	public static void main( String[] args)
+	{
+	
+		GradeBook myGradeBook = new GradeBook();
+
+		myGradeBook.displayMessage();
+	}
+}
