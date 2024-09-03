f3c68e86bf320a0aa8e670387d254f9d5cd541d7
manodhiambo <manodhiambo@gmail.com>
2024-09-03 19:11:15 +0300

    JOptionPane

---
+++ Dialog1.java
@@ -0,0 +1,13 @@
+// Using JOptionPanel to display multiple lines in a dialog
+
+import javax.swing.JOptionPane;
+
+public class Dialog1
+{
+
+	public static void main(String[] args)
+	{
+	
+		JOptionPane.showMessageDialog(null, "Welcome\n to\n Java");
+	}
+}
